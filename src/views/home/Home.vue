fdc4c5129035fe713e4b289853a7b6b1487d6903
Levi <zengyin.ssr@gmail.com>
2023-03-06 07:28:09 +0800

    add axios request modules and add Rotating image

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,21 +1,47 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav">
-      <div slot="left">左边</div>
       <div slot="center">购物街</div>
-      <div slot="right">右边</div>
     </nav-bar>
+    <swiper>
+      <swiper-item v-for="item in banners" :key="item.id">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
 
 import NavBar from 'components/common/navbar/NavBar';
+import {getHomeMultidata} from "network/home";
+// import {Swiper} from "components/common/swiper";
+// import {SwiperItem} from "components/common/swiper";
+import {Swiper, SwiperItem} from 'components/common/swiper'
 
 export default {
   name: "Home",
   components: {
-    NavBar
+    NavBar,
+    Swiper,
+    SwiperItem
+  },
+  data() {
+    // 把请求的数据保存
+    return {
+      banners: [],
+      recommends: []
+    }
+  },
+  created() {
+    // 1. 请求多个数据
+    getHomeMultidata().then(res => {
+      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    })
   }
 }
 </script>
