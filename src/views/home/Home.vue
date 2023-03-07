adf31bd4ef9b266dc503ffab135f8990021626cf
withLevi <zengyin.ssr@gmail.com>
2023-03-07 11:13:59 +0800

    add celling function

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,29 +5,83 @@
     </nav-bar>
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
-    <FeatureView/>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"/>
 
     <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li>
-      <li>8</li>
-      <li>9</li>
-      <li>10</li>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表21</li>
+      <li>列表22</li>
+      <li>列表23</li>
+      <li>列表24</li>
+      <li>列表25</li>
+      <li>列表26</li>
+      <li>列表27</li>
+      <li>列表28</li>
+      <li>列表29</li>
+      <li>列表30</li>
+      <li>列表31</li>
+      <li>列表32</li>
+      <li>列表33</li>
+      <li>列表34</li>
+      <li>列表35</li>
+      <li>列表36</li>
+      <li>列表37</li>
+      <li>列表38</li>
+      <li>列表39</li>
+      <li>列表40</li>
+      <li>列表41</li>
+      <li>列表42</li>
+      <li>列表43</li>
+      <li>列表44</li>
+      <li>列表45</li>
+      <li>列表46</li>
+      <li>列表47</li>
+      <li>列表48</li>
+      <li>列表49</li>
+      <li>列表50</li>
+      <li>列表51</li>
+      <li>列表52</li>
+      <li>列表53</li>
+      <li>列表54</li>
+      <li>列表55</li>
+      <li>列表56</li>
+      <li>列表57</li>
+      <li>列表58</li>
+      <li>列表59</li>
+      <li>列表60</li>
     </ul>
   </div>
 </template>
 
 <script>
 
-import NavBar from 'components/common/navbar/NavBar';
+
 import HomeSwiper from "./childComps/HomeSwiper";
 import RecommendView from "./childComps/RecommendView";
 import FeatureView from "./childComps/FeatureView";
+
+import NavBar from 'components/common/navbar/NavBar';
+import TabControl from "components/content/tabControl/TabControl";
+
 import {getHomeMultidata} from "network/home";
 
 
@@ -38,10 +92,11 @@ import {getHomeMultidata} from "network/home";
 export default {
   name: "Home",
   components: {
-    NavBar,
     HomeSwiper,
     RecommendView,
-    FeatureView
+    FeatureView,
+    NavBar,
+    TabControl
   },
   data() {
     // 把请求的数据保存
@@ -77,5 +132,12 @@ export default {
   right: 0;
   top: 0;
   z-index: 9;
+}
+
+/*实现吸顶的效果*/
+.tab-control {
+  position: sticky;
+  top: 45px;
+
 }
 </style>
