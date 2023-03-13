3ba87597479fb0e98b10f34b5bcafeaf05c04bb6
withLevi <zengyin.ssr@gmail.com>
2023-03-13 13:36:35 +0800

    update activeColor is pink from MainTabbar

--- src/components/content/maintabbar/MainTabbar.vue
+++ src/components/content/maintabbar/MainTabbar.vue
@@ -1,6 +1,6 @@
 <template>
   <tab-bar>
-    <tab-bar-item path="/home" activeColor="red">
+    <tab-bar-item path="/home" activeColor="pink">
       <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="">
       <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
       <div slot="item-text">首页</div>
@@ -12,13 +12,13 @@
       <div slot="item-text">分类</div>
     </tab-bar-item>
 
-    <tab-bar-item path="/cart" activeColor="green">
+    <tab-bar-item path="/cart" activeColor="pink">
       <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg" alt="">
       <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.svg" alt="">
       <div slot="item-text">购物车</div>
     </tab-bar-item>
 
-    <tab-bar-item path="/profile" activeColor="orange">
+    <tab-bar-item path="/profile" activeColor="pink">
       <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="">
       <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" alt="">
       <div slot="item-text">我的</div>
