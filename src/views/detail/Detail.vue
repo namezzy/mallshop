106e6b8b5044729d6017f6e7b07250dda89ec92f
withLevi <zengyin.ssr@gmail.com>
2023-03-29 18:15:04 +0800

    Encapsulation of Navigation Bar

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>{{iid}}</div>
+  <div id="detail">
+    <detail-nav-bar/>
+  </div>
 </template>
 
 <script>
+
+import DetailNavBar from "./childComps/DetailNavBar";
 export default {
   name: "Detail",
+  components: {
+    DetailNavBar
+
+  },
   data() {
     return {
       iid: null
