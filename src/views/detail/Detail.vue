e6ccb4632f5433f6ed43b03af3d558a77c1dc94e
Levi <zengyin.ssr@gmail.com>
2023-04-04 06:34:03 +0800

    Add function: Product detail data display.

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="detail">
     <detail-nav-bar class="detail-nav"/>
-    <scroll class="content">
+    <scroll class="content" ref="scroll">
       <detail-swiper :top-images="topImages">
       </detail-swiper>
       <detail-base-info :goods="goods"/>
       <detail-shop-info :shop="shop"/>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
     </scroll>
   </div>
 </template>
@@ -16,6 +17,7 @@ import DetailNavBar from "./childComps/DetailNavBar";
 import DetailSwiper from "./childComps/DetailSwiper";
 import DetailBaseInfo from "./childComps/DetailBaseInfo";
 import DetailShopInfo from "./childComps/DetailShopInfo";
+import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
 
 import Scroll from "components/common/scroll/Scroll.vue";
 
@@ -28,6 +30,7 @@ export default {
     DetailSwiper,
     DetailBaseInfo,
     DetailShopInfo,
+    DetailGoodsInfo,
     Scroll
   },
   data() {
@@ -35,7 +38,8 @@ export default {
       iid: null,
       topImages: [],
       goods: {},
-      shop: {}
+      shop: {},
+      detailInfo: {}
     }
   },
   created() {
@@ -55,7 +59,15 @@ export default {
       // 创建店铺信息的对象
       this.shop = new Shop(data.shopInfo)
 
+      // 保持商品的详情数据
+      this.detailInfo =  data.detailInfo
+
     })
+  },
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh()
+    }
   }
 }
 </script>
